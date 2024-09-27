8bee01552abfeaa499ac555c4cb752eccf52ca4a
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-27 20:48:15 +0530

    Update LFSR_tb.v

--- LFSR.srcs/sim_1/new/LFSR_tb.v
+++ LFSR.srcs/sim_1/new/LFSR_tb.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.09.2024 20:36:07
-// Design Name: 
-// Module Name: LFSR_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-// Description: Simple Testbench for LFSR.  Set NUM_BITS to different
-// values to verify operation of LFSR
+//Set NUM_BITS to different values to verify the operation of LFSR
 module LFSR_TB ();
 
   parameter NUM_BITS = 4;
